b84f48981cb43b133bab154f09c89f990a5aa0d1
Marco <marco-hilhorst@hotmail.co.nz>
2022-09-11 09:15:06 +1200

    string to number

---
+++ CC-11-09-2022/app.js
@@ -0,0 +1,5 @@
+//write a function that turns a string into a number. All inputs will be strings and every string will be a valid representation of an integral number
+
+const stringToNumber = function(str){
+  return Number(str)
+}
