4bc611fbd2555615e3d74a9be29940d86af80751
Marco <marco-hilhorst@hotmail.co.nz>
2022-09-12 09:12:13 +1200

    greets a name

---
+++ CC-12-09-2022/app.js
@@ -0,0 +1,6 @@
+//Write a function that takes in a name (string) as a parameter and greets the person
+//E.G "Hello, <person> how are oyu doing today."
+
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
