e369df79743c66195dbcde627a57b0e1539f2603
Marco <marco-hilhorst@hotmail.co.nz>
2022-12-17 09:51:16 +1300

    filter array for string with length of 4

---
+++ CC-17-12-2022/app.js
@@ -0,0 +1,6 @@
+//Write a function that takes an array of strings and returns an array containing strings that are exactly 4 scharacters long. The original order should be kept for the output
+
+
+function friend(friends){
+    return friends.filter(names => names.length === 4)
+   }
