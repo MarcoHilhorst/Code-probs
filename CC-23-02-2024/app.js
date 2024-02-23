121ff1b81fd80da7740f676e57d45e3cbfa90719
Marco <marco-hilhorst@hotmail.co.nz>
2024-02-23 17:25:38 +1300

    returns an array of integers from a given number

---
+++ CC-23-02-2024/app.js
@@ -0,0 +1,12 @@
+// uild a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+const reverseSeq = n => {
+    let ans = []
+    while(n>0){
+      ans.push(n)
+      n -= 1
+    }
+    return ans
+  };
